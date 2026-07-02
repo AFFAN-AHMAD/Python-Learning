8648a537c6a94d0a1550c2bf4d3d0ecc92e4cad1
Affan <affanahmad2797@gmail.com>
2026-07-02 23:04:49 +0530

    created pipeline for fetching data

--- generators/large_data_handling/index.py
+++ generators/large_data_handling/index.py
@@ -1,7 +1,35 @@
+"""
+Let’s think of a strategy:
+
+Read every line of the file.
+Split each line into a list of values.
+Extract the column names.
+Use the column names and lists to create a dictionary.
+Filter out the rounds you aren’t interested in.
+Calculate the total and average values for the rounds you are interested in.
+
+"""
 file_name = "techcrunch.csv"
 lines = (line for line in open(file_name))
 # g = next(lines)
 # print(g)
 list_line = (s.rstrip().split(",") for s in lines)
-g = next(list_line)
+# g = next(list_line)
+# print(g)
+
+cols = next(list_line)
+company_dicts = (dict(zip(cols, data)) for data in list_line)
+# g = next(company_dicts)
+# print(g)
+# # print(g)
+
+funding = (
+    int(company_dict["raisedAmt"])
+    for company_dict in company_dicts
+    if company_dict["round"] == "a"
+)
+g = next(funding)
 print(g)
+
+total_series_a = sum(funding)
+print(f"Total series A fundraising: ${total_series_a}")
